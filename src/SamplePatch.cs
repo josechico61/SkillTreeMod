faa197d43dd46da3a884331ea00fd7d49b309da5
josechico61 <josechico61@gmail.com>
2025-10-25 21:33:10 -0600

    Structure updated, references added

--- src/SamplePatch.cs
+++ src/SamplePatch.cs
@@ -1,16 +1,15 @@
-using HarmonyLib;
+﻿using HarmonyLib;
 using UnityEngine;
 
 namespace SkillTreeMod
 {
-    // Example: runs once when the game loads the main menu
-    [HarmonyPatch(typeof(uGUIWindowMainMenu), "OnOpen")]
+    // Runs when the game manager wakes up (very early in startup)
+    [HarmonyPatch(typeof(GameManager), "Awake")]
     public static class SamplePatch
     {
         static void Postfix()
         {
-            Debug.Log("[SkillTreeMod] Loaded! (SamplePatch ran)");
-            // Later: initialize our skill tree data here
+            Debug.Log("[SkillTreeMod] Loaded! (GameManager.Awake patched)");
         }
     }
 }
